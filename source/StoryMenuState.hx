2b4133396ed9d1126d02c3ad4c51d6fb53d63666
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-05-21 14:47:00 -0400

    fuck me sideways

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -30,7 +30,7 @@ class StoryMenuState extends MusicBeatState
 		['Pico', 'Philly Nice', "Blammed"],
 		['Satin Panties', "High", "Milf"],
 		['Cocoa', 'Eggnog', 'Winter Horrorland'],
-		['Senpai', 'Roses', 'Thorns']
+		['Senpai', 'Roses', 'Thorns'],
 		['Our World', 'Mischief']
 	];
 	var curDifficulty:Int = 1;
