d201b11f5bf773a61d25343844bd271f15f94a9b
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-05-21 17:07:55 -0400

    fuck
    fixed fuckup that i fixed but then unfixed while fixing another fuckup. basically i fucked up

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -38,7 +38,7 @@ class HealthIcon extends FlxSprite
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
 		animation.add('quincy', [26, 27], 0, false, isPlayer);
-		animation.add('quincy', [28, 29], 0, false, isPlayer);
+		animation.add('petula', [28, 29], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
