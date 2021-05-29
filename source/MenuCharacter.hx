081d94b4924c98924c83d65e86c67a0a0bfc45d2
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-05-29 15:29:33 -0400

    FUCK OFFF

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -30,7 +30,7 @@ class MenuCharacter extends FlxSprite
 		'mom' => new CharacterSetting(-30, 140, 0.85),
 		'parents-christmas' => new CharacterSetting(100, 130, 1.8),
 		'senpai' => new CharacterSetting(-40, -45, 1.4),
-		'firstweek' => new CharacterSetting(100, 130, 1.8)
+		'firstweek' => new CharacterSetting(100, 130, 1.8),
 		'secondweek' => new CharacterSetting(100, 130, 1.8)
 	];
 
