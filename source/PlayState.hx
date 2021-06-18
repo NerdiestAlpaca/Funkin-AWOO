e84f725c87a8781070638192f3a7e15a8acb085b
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-06-18 11:56:55 -0400

    ok

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1499,9 +1499,9 @@ class PlayState extends MusicBeatState
 		curSong = songData.song;
 
 		if (storyDifficulty == 3) and (SONG.needsVoices);
-			vocals = new FlxSound().loadEmbedded(Paths.voicesEX(PlayState.SONG.song));
+			vocals = new FlxSound().loadEmbedded(Paths.voicesEX(PlayState.SONG.song))
 		else if (curDifficulty < 3) and (SONG.needsVoices);
-			vocals = new FlxSound().loadEmbedded(Paths.voices(PlayState.SONG.song));
+			vocals = new FlxSound().loadEmbedded(Paths.voices(PlayState.SONG.song))
 		else
 			vocals = new FlxSound();
 		
