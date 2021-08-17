54d26c862f1766b808d9862424e4bffc406bb34f
Warren <zoetrent4smash@gmail.com>
2021-08-16 21:46:24 -0400

    i want to kill myself

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -6,7 +6,7 @@ using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD", "REMATCH"];
+	public static var difficultyArray:Array<String> = ['Easy', "Normal", "Hard", "Rematch"];
 
 	public static function difficultyString():String
 	{
