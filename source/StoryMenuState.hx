183e4e3425362a9d9151c1fdf1afa45171641256
Warren <zoetrent4smash@gmail.com>
2021-08-29 18:11:35 -0400

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -31,7 +31,7 @@ class StoryMenuState extends MusicBeatState
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true];
 
 	var weekCharacters:Array<Dynamic> = [
 		['lps', 'bf', 'gf'],
