ee5e6a8d4d9e304449ee383e6464cc76f7279889
Warren <zoetrent4smash@gmail.com>
2021-08-29 14:41:05 -0400

    Update modchart.lua

--- assets/preload/data/mischief/modchart.lua
+++ assets/preload/data/mischief/modchart.lua
@@ -12,6 +12,16 @@ end
 
 function update (elapsed)
     local currentBeat = (songPos / 1000)*(bpm/95)
+    if resetnotes then
+        for i = 0, 7 do
+            setActorX(_G['defaultStrum'..i..'X'] + 0, i)
+            setActorY(_G['defaultStrum'..i..'Y'] + 10,i)
+            setActorAngle(0, i)
+            camHudAngle = 0
+            setHudPosition(0, 0)
+            cameraAngle = 0
+        end
+    end
     if difficulty == 2 then -- we're on hard mode
         if swayingstart then
             for i=0,7 do
@@ -51,25 +61,28 @@ function update (elapsed)
         end
         if swayingy then
             for i=0,7 do
-                setActorY(defaultStrum0Y + 15 * math.cos((currentBeat + i*0.3) * 1.5), i)
+                setActorY(defaultStrum0Y + 12 * math.cos((currentBeat + i*0.25) * 1.5), i)
+                setActorX(_G['defaultStrum'..i..'X'] + 32 * math.sin(currentBeat + i*1), i)
             end
         end
         if swayingx then
             for i=0,7 do
-                setActorX(_G['defaultStrum'..i..'X'] + 20 * math.sin((currentBeat + i*0.01)), i)
+                setActorX(_G['defaultStrum'..i..'X'] + 50 * math.sin((songPos / 1000) * math.pi), i)
+                setActorY(_G['defaultStrum'..i..'Y'] + 30 * math.sin((songPos / 1000) * 2 * math.pi), i)
             end
         end
         if swayingboth then
             for i=0,7 do
-                setActorY(defaultStrum0Y + 15 * math.cos((currentBeat + i*0.3) * 1.5), i)
-                setActorX(_G['defaultStrum'..i..'X'] + 20 * math.sin((currentBeat + i*0.01)), i)
+                setActorY(_G['defaultStrum'..i..'Y'] + 30 * math.sin((songPos / 1000) * 2 * math.pi), i)
+                setActorX(_G['defaultStrum'..i..'X'] + 32 * math.sin(currentBeat + i*1.2), i)
             end
         end
         if swayingintense then
-            local currentBeat = (songPos / 1000)*(bpm/70)
+            local currentBeat = (songPos / 500)*(bpm/60)
+            camHudAngle = 1 * math.cos((currentBeat))
             for i=0,7 do
-                setActorY(defaultStrum0Y + 18 * math.cos((currentBeat + i*0.25) * 1.8), i)
-                setActorX(_G['defaultStrum'..i..'X'] + 24 * math.sin((currentBeat + i*0.02)), i)
+                setActorY(_G['defaultStrum'..i..'Y'] + 32 * math.sin(currentBeat + i*1), i)
+                setActorX(_G['defaultStrum'..i..'X'] + 50 * math.sin((songPos / 1000) * math.pi), i)
             end
         end
     end
@@ -183,5 +196,62 @@ end
 end
 
 function stepHit (step)
-
+    if difficulty == 3 then
+        if step == 1 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) + 360, 0.5)
+        end
+    end
+    if step == 16 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) - 360, 0.5)
+        end
+    end
+    if step == 32 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) + 360, 0.5)
+        end
+    end
+    if step == 48 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) - 360, 0.5)
+        end
+    end
+    if step == 64 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) + 360, 0.5)
+        end
+    end
+    if step == 80 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) - 360, 0.5)
+        end
+    end
+    if step == 96 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) + 360, 0.5)
+        end
+    end
+    if step == 112 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) - 360, 0.5)
+        end
+    end
+    if step == 120 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) + 360, 0.2)
+        end
+    end
+    if step == 124 then
+        for i = 0, 7 do
+            tweenPosXAngle(i, _G['defaultStrum'..i..'X'] - 0 ,getActorAngle(i) - 360, 0.2)
+        end
+    end
+        if step == 383 then
+            resetnotes = true
+        end
+        if step == 384 then
+            resetnotes = false
+        end
+    end
 end
